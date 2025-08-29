eeb9e49112e540d3d611e6b65835e5ee28100132
NgocTT <ngoctt@a-livesystems.com>
2025-08-29 12:47:38 +0700

    upcode

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp        	= require('gulp'),
+      browsersync 	= require('browser-sync').create();
+
+
+
+// BrowserSync
+function browserSync(done){
+	browsersync.init({
+    server: {
+      baseDir: "./",
+      directory: true
+    },
+    open: true
+  });
+	done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Watch files
+function watcher() {
+  gulp.watch(['./**/*.html', './assets/js/*.js', './assets/css/*.css'], browserSyncReload);
+}
+
+// export tasks
+exports.default = gulp.parallel(watcher, browserSync);
